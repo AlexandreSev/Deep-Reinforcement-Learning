83510dcdd218f50322231f912ec3bbbbc87efdf7
Nicolas Hennetier <nicolashennetier2@gmail.com>
2017-04-21 21:50:11 +0200

    New environment

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ if __name__=="__main__":
             render=False, master=False, env_name="CartPole-v1", goal=495, learning_rate=0.001, 
             weighted=False, algo="a3c", eps_fall=10000, callback=True)
     else:
-        main(8, T_max=10000000, model_option={"n_hidden":2, "hidden_size":[128, 256]}, 
-            render=False, master=False, env_name="CartPole-v1", goal=495, learning_rate=0.001, 
+        main(16, T_max=10000000, model_option={"n_hidden":2, "hidden_size":[128, 256]}, 
+            render=False, master=False, env_name="MountainCar-v0", goal=495, learning_rate=0.001, 
             weighted=False, algo="a3c", eps_fall=10000, callback=True, Itarget=100, action_replay=1, 
             reset=True)
